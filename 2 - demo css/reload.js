bef7e57f5fc127a405038dfd1f90917b801b8a23
Simon LOIR <simon.loir@student.unamur.be>
2022-09-08 14:36:42 +0200

    initial commit

---
+++ 2 - demo css/reload.js
@@ -0,0 +1,20 @@
+let last = {},
+    files = [
+        window.location.href,
+        ...Array.from(document.querySelectorAll('link[rel="stylesheet"]')).map(
+            (e) => e.href
+        ),
+        ...Array.from(document.querySelectorAll('script')).map((e) => e.src),
+    ];
+
+document.addEventListener('DOMContentLoaded', async () => {
+    setInterval(async () => {
+        for (let i = 0; i < files.length; i++) {
+            const filename = files[i];
+            const data = await fetch(filename).then((e) => e.text());
+            if (last[filename] && last[filename] !== data)
+                return window.location.reload();
+            last[filename] = data;
+        }
+    }, 2000);
+});
